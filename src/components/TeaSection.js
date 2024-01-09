3442436dadb101b067a528fb48b0252623416b9d
RootBeer8 <109646119+RootBeer8@users.noreply.github.com>
2024-01-09 11:50:07 -0500

    update 1.9

--- src/components/TeaSection.js
+++ src/components/TeaSection.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import '../App.css';
+import { Button } from './Button';
+import './TeaSection.css';
+import Video from '../videos/video-2.mp4';
 
 function TeaSection() {
   return (
     <div className = 'tea-container'>
-      <video>src = '/videos/video-1.mp4' autoPlay loop muted</video>
+      <video src={Video} autoPlay loop muted/>
+      <h1>Welcome to Bob's Boba!</h1>
+      <p>Have you tried our newest drink flavors?</p>
+      <div className='tea-btns'>
+        <Button className = 'btns' buttonStyle = 'btn--outline' buttonSize = 'btn--large'>GET STARTED
+        </Button>
+        <Button className = 'btns' buttonStyle = 'btn--primary' buttonSize = 'btn--large'>ABOUT US
+        </Button>
+      </div>
     </div>
   )
 }
---
+++ src/components/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../App.css';
+import TeaSection from '../TeaSection';
+
+function Home () {
+    return (
+        <>
+           <TeaSection>
+            </TeaSection> 
+        </>
+    )
+}
+
+export default Home;
