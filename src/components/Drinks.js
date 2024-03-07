5823f28d6ac6aaa769c9bc404873333704ae8ec7
RootBeer8 <109646119+RootBeer8@users.noreply.github.com>
2024-03-06 23:55:01 -0500

    Update the styles of the drink menu

--- src/components/Drinks.js
+++ src/components/Drinks.js
@@ -3,37 +3,50 @@ import './Drinks.css';
 
 function Drinks() {
     return (
-        <div className="menuItems">
-            <h2>Boba Drinks:</h2>
 
-            <h4>Thai Tea</h4>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati voluptatibus voluptatum distinctio ipsam! Sapiente, nulla!</p>
+        <div className="drinks">
 
-            <h4>Chai</h4>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati voluptatibus voluptatum distinctio ipsam! Sapiente, nulla!</p>
+                <h1>Boba Drinks:</h1>
+                    <div className="menuItems">
 
-            <h4>Matcha Tea</h4>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati voluptatibus voluptatum distinctio ipsam! Sapiente, nulla!</p>
+                    <div className="drink">
+                        <h3>Thai Tea</h3>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati voluptatibus voluptatum distinctio ipsam! Sapiente, nulla!</p>
+                    </div>
 
-            <h4>Lychee Tea</h4>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati voluptatibus voluptatum distinctio ipsam! Sapiente, nulla!</p>
+                    <div className="drink">
+                        <h3>Lychee Tea</h3>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati voluptatibus voluptatum distinctio ipsam! Sapiente, nulla!</p>
+                    </div>
 
-            <h4>Tiger Tea</h4>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati voluptatibus voluptatum distinctio ipsam! Sapiente, nulla!</p>
+                    <div className="drink">
+                        <h3>Jasmine Tea</h3>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati voluptatibus voluptatum distinctio ipsam! Sapiente, nulla!</p>
+                    </div>
 
-            <h4>Taro Tea</h4>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati voluptatibus voluptatum distinctio ipsam! Sapiente, nulla!</p>
+                    <div className="drink">
+                        <h3>Chai</h3>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati voluptatibus voluptatum distinctio ipsam! Sapiente, nulla!</p>
+                    </div>
 
-            <h4>Grass Jelly</h4>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati voluptatibus voluptatum distinctio ipsam! Sapiente, nulla!</p>
+                    <div className="drink">
+                        <h3>Match Tea</h3>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati voluptatibus voluptatum distinctio ipsam! Sapiente, nulla!</p>
+                    </div>
+                    
+                    <div className="drink">
+                        <h3>Tarro Tea</h3>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati voluptatibus voluptatum distinctio ipsam! Sapiente, nulla!</p>
+                    </div>
 
-            <h4>Green Tea</h4>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati voluptatibus voluptatum distinctio ipsam! Sapiente, nulla!</p>
+                    <div className="drink">
+                        <h3>Earl Grey Tea</h3>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati voluptatibus voluptatum distinctio ipsam! Sapiente, nulla!</p>
+                    </div>
 
-            <h4>Jasmine Tea</h4>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati voluptatibus voluptatum distinctio ipsam! Sapiente, nulla!</p>
+            </div>
+    </div>
 
-        </div>
     )
 }
 
