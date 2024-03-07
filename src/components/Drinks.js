3729b64b6ae5c7b4bc9a0d6ad75aec15b30aaf93
RootBeer8 <109646119+RootBeer8@users.noreply.github.com>
2024-03-06 23:23:11 -0500

    Added menu list

---
+++ src/components/Drinks.js
@@ -0,0 +1,40 @@
+import React from "react";
+import './Drinks.css';
+
+function Drinks() {
+    return (
+        <div className="menuItems">
+            <h2>Boba Drinks:</h2>
+
+            <h4>Thai Tea</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati voluptatibus voluptatum distinctio ipsam! Sapiente, nulla!</p>
+
+            <h4>Chai</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati voluptatibus voluptatum distinctio ipsam! Sapiente, nulla!</p>
+
+            <h4>Matcha Tea</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati voluptatibus voluptatum distinctio ipsam! Sapiente, nulla!</p>
+
+            <h4>Lychee Tea</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati voluptatibus voluptatum distinctio ipsam! Sapiente, nulla!</p>
+
+            <h4>Tiger Tea</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati voluptatibus voluptatum distinctio ipsam! Sapiente, nulla!</p>
+
+            <h4>Taro Tea</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati voluptatibus voluptatum distinctio ipsam! Sapiente, nulla!</p>
+
+            <h4>Grass Jelly</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati voluptatibus voluptatum distinctio ipsam! Sapiente, nulla!</p>
+
+            <h4>Green Tea</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati voluptatibus voluptatum distinctio ipsam! Sapiente, nulla!</p>
+
+            <h4>Jasmine Tea</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati voluptatibus voluptatum distinctio ipsam! Sapiente, nulla!</p>
+
+        </div>
+    )
+}
+
+export default Drinks;
--- src/components/pages/Menu.js
+++ src/components/pages/Menu.js
@@ -1,11 +1,13 @@
 import React from "react";
 import '../../App.css';
 import Footer from '../Footer';
+import Drinks from '../Drinks';
 
 export default function Menu() {
     return (
         <>
         <h1 className='menu'>MENU</h1>
+            <Drinks />
            <Footer />
         </>
     )
