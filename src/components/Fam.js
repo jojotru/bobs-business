0a2b68b80674262bfb583e50392bbaa71838bd8a
RootBeer8 <109646119+RootBeer8@users.noreply.github.com>
2024-03-08 00:12:56 -0500

    complete the about section

---
+++ src/components/Fam.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Fam.css";
+
+function Fam() {
+    return (
+        <div className="aboutBkg">
+            <div className="aboutBox">
+                <div className="aboutInfo">
+                    <h1>From Our Family to Yours</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum non libero modi alias veniam maiores ut neque architecto iste blanditiis fugiat mollitia, corrupti id soluta inventore dolorum voluptatibus repudiandae ratione. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis voluptatum, soluta rem eligendi adipisci quisquam omnis distinctio eaque velit enim in quaerat nam ad, accusantium vero neque illo quae voluptatibus!
+                    </p>
+                </div>
+                <div className="aboutPhoto">
+                    <img src="../images/bob.jpeg" alt="Bob" className="aboutImg"/>
+                    <img src="../images/bob2.jpeg" alt="Bob" className="aboutImg imgH"/>
+                    <img src="../images/bob3.jpeg" alt="Bob" className="aboutImg imgH"/>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Fam;
--- src/components/pages/AboutUs.js
+++ src/components/pages/AboutUs.js
@@ -1,11 +1,13 @@
 import React from "react";
 import '../../App.css';
 import Footer from '../Footer';
+import Fam from '../Fam';
 
 export default function AboutUs() {
     return (
         <>
         <h1 className='about-us'>ABOUT US</h1>
+            <Fam />
            <Footer />
         </>
     );
