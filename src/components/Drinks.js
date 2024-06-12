6ab3071c5daf2c9b3f8a1752f962bca8d23c4389
RootBeer8 <109646119+jojotru@users.noreply.github.com>
2024-06-11 23:19:00 -0400

    Revert "Merge branch 'gh-pages' of https://github.com/jojotru/bobs-business into gh-pages"
    This reverts commit a8e59f5377d9d61e98a41091891bbf121c131da6, reversing
    changes made to f48ccc3eca6eb2be7c94c39e3edb7f28bbfa790b.

---
+++ src/components/Drinks.js
@@ -0,0 +1,53 @@
+import React from "react";
+import './Drinks.css';
+
+function Drinks() {
+    return (
+
+        <div className="drinks">
+
+                <h1>Boba Drinks:</h1>
+                    <div className="menuItems">
+
+                    <div className="drink">
+                        <h3>Thai Tea</h3>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati voluptatibus voluptatum distinctio ipsam! Sapiente, nulla!</p>
+                    </div>
+
+                    <div className="drink">
+                        <h3>Lychee Tea</h3>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati voluptatibus voluptatum distinctio ipsam! Sapiente, nulla!</p>
+                    </div>
+
+                    <div className="drink">
+                        <h3>Jasmine Tea</h3>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati voluptatibus voluptatum distinctio ipsam! Sapiente, nulla!</p>
+                    </div>
+
+                    <div className="drink">
+                        <h3>Chai</h3>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati voluptatibus voluptatum distinctio ipsam! Sapiente, nulla!</p>
+                    </div>
+
+                    <div className="drink">
+                        <h3>Match Tea</h3>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati voluptatibus voluptatum distinctio ipsam! Sapiente, nulla!</p>
+                    </div>
+                    
+                    <div className="drink">
+                        <h3>Tarro Tea</h3>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati voluptatibus voluptatum distinctio ipsam! Sapiente, nulla!</p>
+                    </div>
+
+                    <div className="drink">
+                        <h3>Earl Grey Tea</h3>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati voluptatibus voluptatum distinctio ipsam! Sapiente, nulla!</p>
+                    </div>
+
+            </div>
+    </div>
+
+    )
+}
+
+export default Drinks;
---
+++ src/components/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../../App.css';
+import TeaSection from '../TeaSection';
+import Cards from '../Cards';
+import Footer from '../Footer';
+
+function Home () {
+    return (
+        <>
+           <TeaSection />
+           <Cards/>
+           <Footer />
+        </>
+    )
+}
+
+export default Home;
