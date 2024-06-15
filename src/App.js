32166b28abd524cfb01643f37b2de0a4a434a219
RootBeer8 <109646119+jojotru@users.noreply.github.com>
2024-06-14 23:39:40 -0400

    .

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Routes, Route } from 'react-router-dom';
 
-
 import './App.css';
 import Home from './components/pages/Home';
 import Menu from './components/pages/Menu';
