1a6bf88b0dcb2b821752e34fe9b3428f577a9c78
RootBeer8 <109646119+jojotru@users.noreply.github.com>
2024-06-17 00:41:31 -0400

    .

--- src/components/Fam.js
+++ src/components/Fam.js
@@ -11,8 +11,8 @@ function Fam() {
                     </p>
                 </div>
                 <div className="aboutPhoto">
-                    <img src="../images/bob.jpeg" alt="Bob" className="aboutImg"/>
-                    <img src="././images/bob2.jpeg" alt="Bob" className="aboutImg imgH"/>
+                    <img src="/bobs-business/images/bob.jpeg" alt="Bob" className="aboutImg"/>
+                    <img src="bobs-business/images/bob2.jpeg" alt="Bob" className="aboutImg imgH"/>
                     <img src="./images/bob3.jpeg" alt="Bob" className="aboutImg imgH"/>
                 </div>
             </div>
